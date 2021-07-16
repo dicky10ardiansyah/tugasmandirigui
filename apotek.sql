a6d9ace110da34a7c75ebf242a745e963c78a388
Dicky Ardiansyah <eigerdlamborghini.ardiansyah@gmail.com>
2021-07-16 08:07:10 +0700

    tugas mandiri gui

---
+++ apotek.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2021 at 03:05 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `kode_obat` varchar(10) NOT NULL,
+  `nama_obat` varchar(30) NOT NULL,
+  `id_kategori` varchar(10) NOT NULL,
+  `kode_satuan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`kode_obat`, `nama_obat`, `id_kategori`, `kode_satuan`) VALUES
+('', '', '', ''),
+('01', 'Tuzalos', '001', '061'),
+('02', 'Paramex', '001', '062'),
+('03', 'Bodrex', '001', '063'),
+('04', 'Entrostop', '023', '075'),
+('98', 'sdfr', '837', '8');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`kode_obat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
